2b77c0e6da4112d167c2f9f612f8aa800d03bfd0
narvis2 <narvis2@douzone.com>
2023-08-04 11:35:29 +0900

    🌸 로그아웃 DTO Validation Message 추가 🌸
    - 로그아웃 시 들어오는 accessToken 에 validation message 추가

--- src/main/kotlin/com/thepan/reservationapiserver/domain/sign/dto/SignOutRequest.kt
+++ src/main/kotlin/com/thepan/reservationapiserver/domain/sign/dto/SignOutRequest.kt
@@ -3,6 +3,6 @@ package com.thepan.reservationapiserver.domain.sign.dto
 import jakarta.validation.constraints.NotBlank
 
 data class SignOutRequest(
-    @field:NotBlank
+    @field:NotBlank(message = "accessToken 이 존재하지 않습니다.")
     val accessToken: String
 )
